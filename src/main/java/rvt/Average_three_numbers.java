304180cc0f46fe3519113cdd3ebabeb1565c4f4e
24DP1PBerz <a240332pb@rvt.lv>
2025-09-16 14:42:01 +0300

    Addition formula

---
+++ src/main/java/rvt/Average_three_numbers.java
@@ -0,0 +1,26 @@
+package rvt;
+import java.util.Scanner;
+
+public class Average_three_numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number: ");
+        int first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number: ");
+        int second = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number: ");
+        int third = Integer.valueOf(scanner.nextLine());
+
+        double sum = (first + second + third) / 3.0;
+
+        System.out.println("The average is " + sum);
+
+        scanner.close();
+
+    }
+    
+
+}
