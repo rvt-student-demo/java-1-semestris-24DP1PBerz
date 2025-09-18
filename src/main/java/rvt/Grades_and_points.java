9e7d200581a356cf9ab75fcc7239e6aaee4311eb
24DP1PBerz <a240332pb@rvt.lv>
2025-09-18 12:37:22 +0300

    Grades and Points 1-6( pievienots "grade" pie system printiem)

--- src/main/java/rvt/Grades_and_points.java
+++ src/main/java/rvt/Grades_and_points.java
@@ -8,21 +8,21 @@ public class Grades_and_points {
         int num = Integer.valueOf(scanner.nextLine());
 
         if (num < 0) {
-            System.out.println("Impossible!");
-        } else if (num > 0 &&num <= 49) {
-            System.out.println("failed");
+            System.out.println("Grade: Impossible!");
+        } else if (num > 0 && num <= 49) {
+            System.out.println("Grade: failed");
         } else if (num > 49 && num < 60){
-            System.out.println("1");
+            System.out.println("Grade: 1");
         } else if (num > 59 && num < 70) {
-            System.out.println("2");
+            System.out.println("Grade: 2");
         } else if (num > 69 && num < 80) {
-            System.out.println("3");
+            System.out.println("Grade: 3");
         } else if (num > 79 && num < 90) {
-            System.out.println("4");
+            System.out.println("Grade: 4");
         } else if (num > 89 && num < 101) {
-            System.out.println("5");
+            System.out.println("Grade: 5");
         }else{
-            System.out.println("incredible!");
+            System.out.println("Grade: incredible!");
         }
 
         scanner.close();
